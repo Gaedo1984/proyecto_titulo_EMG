9c8468ada93dd07af635631512b8503b4ec1dfda
Gustavo <gustavo_aedo@live.com>
2022-11-27 20:32:38 -0300

    se agrega h2 a home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
         <main className="payment">
             <i className="fa-solid fa-truck-medical"></i>
             <h1>Bienvenidos a las patologías radiológicas</h1>
+            <h2>Técnica radiológica por medio de scanner</h2>
         </main>
     )
 }
