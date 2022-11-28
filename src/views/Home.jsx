e72c0e331865f0be53cbdd7d0f4ce76e795785d0
Gustavo <gustavo_aedo@live.com>
2022-11-27 22:34:51 -0300

    se agrega foto en home y se corrigen patologias

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
             <i className="fa-solid fa-truck-medical"></i>
             <h1>Bienvenidos a las patologías radiológicas</h1>
             <h2>Técnica radiológica por medio de scanner</h2>
-            <img src="..\src\assets\img\interno_low.png" alt="" />
+            <img src="https://tuguiadiagnostica.cl/wp-content/uploads/2022/11/Imagen-portada-pag_low.png" alt="" />
         </main>
     )
 }
