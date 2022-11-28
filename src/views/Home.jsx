ba941d730790784106a65286effe1151e3b11531
Gustavo <gustavo_aedo@live.com>
2022-11-28 10:46:27 -0300

    Camb io en patologias y se agrega mensaje en home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
             <i className="fa-solid fa-truck-medical"></i>
             <h1>Bienvenidos a las patologías radiológicas</h1>
             <h2>Técnica radiológica por medio de scanner</h2>
+            <h3 className="study-zone">Zona de estudio: Abdomen y pelvis</h3>
             <img src="https://tuguiadiagnostica.cl/wp-content/uploads/2022/11/Imagen-portada-pag_low.png" alt="" />
         </main>
     )
